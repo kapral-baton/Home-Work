7be084d12f511a69b50f464ee9dfd1ca4b1c2661
Tima Ivanov <ivtima.bp@gmail.com>
2022-08-05 20:27:31 +0200

    hw_10

--- js/js.js
+++ js/js.js
@@ -1,42 +1,32 @@
-const animals = [
-    ['🐭','mouse','Jerry'],
-    ['🐹','hamster','Biscuit'],
-    ['🐰','rabbit','Bugs'],
-    ['🦊','fox','Mrs. Fox'],
-    ['🐻','bear','Paddington']
+const products = [
+    ['apple',10],
+    ['banana',8],
+    ['mango',20],
+    ['grape',18]
 ];
 
-const food = [
-    ['🍎','apple',10],
-    ['🍐','pear',12],
-    ['🍊','tangerine',15],
-    ['🍋','lemon',5],
-    ['🍌','banana',7]
-];
-
-const universes = [
-    ['🖤', 'DC', ['Superman', 'Batman', 'Wonder Woman']],
-    ['❤️', 'Marvel', ['Iron Man', 'the Hulk', 'Black Widow']]
-]
-
-function getInfo(a,b) {
-    const TRs = [];
-    for(let tr=1; tr<=a.length ; tr++){
-        let TDs = [];
-        let counter = a[tr-1];
-        for(let td=1; td<= counter.length; td++){
-            if(Array.isArray(counter[td-1]) ) {
-                TDs.push(`<td>${counter[td-1].join(';')}</td>`);
-            }else {
-                TDs.push(`<td>${counter[td-1]}</td>`);
-            }
+function getPrice (products,seasonFunc = null) {
+    const copiedProducts = [...products];
+    let suma = 0;
+    for (let i = 0; i < copiedProducts.length;i++) {
+        if (seasonFunc === null ) {
+            suma += copiedProducts[i][1];
+        }else {
+            suma += seasonFunc(copiedProducts[i][1]);
         }
-        TRs.push(`<tr>${TDs.join(``)}</tr>`);
     }
-    document.write(`<table><caption>${b}</caption>${TRs.join(``)}</table>`);
+    return suma;
 }
 
-getInfo(animals , 'Animals info');
-getInfo(food , 'Food info');
-getInfo(universes , 'Universes info');
+function summerValue(value){
+    return value*0.8;
+}
+function winterValue(value){
+    return value*2;
+}
+
+
+console.log(getPrice(products,summerValue));
+console.log(getPrice(products,winterValue));
+console.log(getPrice(products,));
 
