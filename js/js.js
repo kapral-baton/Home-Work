89cd285fda9b338a334db842a1927cf712c80e5a
Tima Ivanov <ivtima.bp@gmail.com>
2022-08-09 20:41:50 +0200

    hw_11

--- js/js.js
+++ js/js.js
@@ -1,33 +1,54 @@
-const products = [
-    ['apple',10],
-    ['banana',8],
-    ['mango',20],
-    ['grape',18]
+const users = [
+    ["john","red",5,["ball", "book", "pen"]],
+    ["becky","blue",10,["tape", "backpack", "pen"]],
+    ["susy","red",55,["ball", "eraser", "pen"]],
+    ["tyson","green",1,["book", "pen"]],
 ];
 
-function getPrice (products,seasonFunc) {
-    const copiedProducts = JSON.stringify(products);
-    let suma = 0;
-    for (let i = 0; i < copiedProducts.length;i++) {
-        if (typeof seasonFunc === `function` ) {
-            suma += copiedProducts[i][1];
-        }else {
-            suma += seasonFunc(copiedProducts[i][1]);
-        }
-    }
-    return suma;
-}
+let array1 = [];
+users.forEach(element => array1.push(element[0]+'!'));
+
+let array2 = users.map(element => element[0]+'?');
+
+let userRed = users.filter(element => element[1] === 'red');
+
+let totalScore = 0;
+userRed.forEach(element => totalScore += element[2]);
+
+let table = '';
+userRed.forEach(element => table +=(`
+        <tr>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td>${element[2]}</td>
+            <td>${element[3].join(';')}</td>
+        </tr>`));
+
+document.write(`<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Comand</th>
+            <th>Score</th>
+            <th>Data</th>
+        </tr>
+    </thead>
+    <tbody>
+           ${table}
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="4">Total score: ${totalScore}</td>
+        </tr>
+    </tfoot>
+</table>`);
 
 
-function summerValue(value){
-    return value*0.8;
-}
-function winterValue(value){
-    return value*2;
-}
 
 
-console.log(getPrice(products,summerValue));
-console.log(getPrice(products,winterValue));
-console.log(getPrice(products,));
+
+
+
+
+
 
