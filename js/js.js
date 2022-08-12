81a2716ba0c82f1f17443f99c6d8d165ab21e559
Tima Ivanov <ivtima.bp@gmail.com>
2022-08-12 18:16:17 +0200

    hw_12

--- js/js.js
+++ js/js.js
@@ -1,54 +1,31 @@
-const users = [
-    ["john","red",5,["ball", "book", "pen"]],
-    ["becky","blue",10,["tape", "backpack", "pen"]],
-    ["susy","red",55,["ball", "eraser", "pen"]],
-    ["tyson","green",1,["book", "pen"]],
-];
+const obj = {
+    x: 10,
+    y: 20,
+    inner: {
+        x: 20,
+        z: 30
+    },
+    foo2: {
+        k: 23,
+        p: 13
+    }
+}
 
-let array1 = [];
-users.forEach(element => array1.push(element[0]+'!'));
+let convert = function(object) {
+    const newObj = {};
+    for (let key in object) {
+        if (typeof object[key] === 'object' ) {
+            let a = object[key];
+            for (let key2 in a) {
+                newObj[key2] = a[key2];
+            }
+        }else {
+            newObj[key] = object[key];
+        }
+    }
+    return newObj;
+};
 
-let array2 = users.map(element => element[0]+'?');
-
-let userRed = users.filter(element => element[1] === 'red');
-
-let totalScore = 0;
-userRed.forEach(element => totalScore += element[2]);
-
-let table = '';
-userRed.forEach(element => table +=(`
-        <tr>
-            <td>${element[0]}</td>
-            <td>${element[1]}</td>
-            <td>${element[2]}</td>
-            <td>${element[3].join(';')}</td>
-        </tr>`));
-
-document.write(`<table>
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Comand</th>
-            <th>Score</th>
-            <th>Data</th>
-        </tr>
-    </thead>
-    <tbody>
-           ${table}
-    </tbody>
-    <tfoot>
-        <tr>
-            <td colspan="4">Total score: ${totalScore}</td>
-        </tr>
-    </tfoot>
-</table>`);
-
-
-
-
-
-
-
-
+console.log(convert(obj));
 
 
