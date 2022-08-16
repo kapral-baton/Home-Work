a41ae634ebeb17e71519e186cfec97ef435ef3f2
Tima Ivanov <ivtima.bp@gmail.com>
2022-08-16 20:13:47 +0200

    hw_13

--- js/js.js
+++ js/js.js
@@ -1,31 +1,49 @@
-const obj = {
-    x: 10,
-    y: 20,
-    inner: {
-        x: 20,
-        z: 30
+const vegetables = [
+    {
+        name: `tomato`,
+        icon: `🍅`,
+        price: 2.3
     },
-    foo2: {
-        k: 23,
-        p: 13
+    {
+        name: `carrot`,
+        icon: `🥕`,
+        price: 1.5
+    },
+    {
+        name: `corn`,
+        icon: `🌽`,
+        price: 2.78,
+        season: true
+    }
+];
+
+class Vegetable{
+    constructor(obj){
+        this.type = "Vegetable";
+        this.seasonKoef = 1.3;
+        this.season = obj.season;
+        this.name = obj.name;
+        this.icon = obj.icon;
+        this.price = obj.price;
+    }
+
+    getPrice(){
+        return this.season ? this.seasonKoef * this.price : this.price;
+    }
+
+    getInfo(){
+        return `Type: Vegetable. SeasonKoef: ${this.seasonKoef}. Name: ${this.name}. Icon: ${this.icon}. Price: ${this.price}  ${this.season === true ? 'Season: true':''}`;
     }
 }
 
-let convert = function(object) {
-    const newObj = {};
-    for (let key in object) {
-        if (typeof object[key] === 'object' ) {
-            let a = object[key];
-            for (let key2 in a) {
-                newObj[key2] = a[key2];
-            }
-        }else {
-            newObj[key] = object[key];
-        }
-    }
-    return newObj;
-};
+let newVegetables = vegetables.map(item => new Vegetable(item));
 
-console.log(convert(obj));
+let info = newVegetables.map(item => item.getInfo());
+
+document.write(`<ul>
+    <li>${info[0]}</li>
+    <li>${info[1]}</li>
+    <li>${info[2]}</li>
+</ul>`);
 
 
