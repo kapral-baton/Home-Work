d095b1379ad3e730f938542ba10f49d4422f458b
Tima Ivanov <ivtima.bp@gmail.com>
2022-10-01 21:35:35 +0200

    hw_25

--- index.js
+++ index.js
@@ -1,36 +1,105 @@
-const select = document.querySelector('#jokesCategories');
+const BASE_URL = 'https://61c9d37520ac1c0017ed8eac.mockapi.io'
+const heroes = [];
 
+const div = document.querySelector('#containerHero');
+const form = document.querySelector('#formHero');
 
-
-fetch('https://api.chucknorris.io/jokes/categories')
-.then(data => data.json())
-    .then(data =>  data.forEach(item => select.innerHTML += `<option>${item}</option>`));
-
-
-select.addEventListener('change', event => {
-    const ul = document.querySelector('#jokesList');
-    fetch(`https://api.chucknorris.io/jokes/random?category=${select.value}`)
+const getUniverses = async () =>{
+    fetch(`${BASE_URL}/universes`)
         .then(data => data.json())
-        .then(data => ul.innerHTML = `<li>
-                                            <p>Category: <b>${select.value}</b></p>
-                                            <p>${data.value}</p>
-                                            <button id="button">Remove joke</button>
-                                        </li>`)
-        .then(() => {
-            select.setAttribute("disabled", "true");
+        .then(data => {
+            const universes = document.querySelector('#comics');
+            universes.innerHTML = data.map(item => `<option value=${item.name}>${item.name}</option>`);
+        });
+}
 
-        })
+const getHeroes = async () => {
+    fetch(`${BASE_URL}/heroes`)
+        .then(data => data.json())
+        .then(data =>  data.forEach(item => heroes.push(item)))
         .then(() => {
-            let button = document.querySelector('#button');
+            let text = '';
+            heroes.forEach(item => text += `<tr id=${item.id}>
+                <td>${item.name}</td>
+                <td>${item.comics}</td>
+                <td >
+                    <input 
+                        type="checkbox" 
+                        ${item.favourite ?'checked': ''} 
+                        id=${item.id} 
+                        class="favouriteCheckbox"
+                    >
+                </td>
+                <td><button id=${item.id} type="delete">Delete</button></td>
+            </tr>`)
+            div.innerHTML += `<table>${text}</table>`;
+            const  checkBox = document.querySelectorAll('#favouriteCheckbox');
+            console.log(checkBox);
 
-            button.addEventListener('click', event => {
-                select.removeAttribute("disabled");
-                document.querySelector('li').remove();
+            checkBox.forEach(elem => {
+                console.log(elem);
+                elem.addEventListener('change', edit)
             })
+            const deleteButton = document.querySelectorAll('button[type="delete"]');
+            console.log('------',deleteButton);
+            deleteButton.forEach( item => item.addEventListener('click', deleteItem))
         });
 
+}
 
-});
+const postHero = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const data = {
+        name: formData.get("name"),
+        comics: formData.get("comics"),
+        favourite: formData.get("favourite") === "on"
+    }
+
+    fetch(`${BASE_URL}/heroes`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+}
+
+const edit = (e) => {
+    fetch(`${BASE_URL}/heroes/${e.target.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ favourite: e.target.checked })
+    })
+}
+
+
+const deleteItem = (e) => {
+    fetch(`${BASE_URL}/heroes/${e.target.id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({ favourite: e.target.checked })
+    })
+        .then(data => data.json())
+        .then(() => {
+            getHeroes()
+        });
+}
+
+
+
+
+getUniverses();
+getHeroes();
+
+form.addEventListener('submit', postHero);
+
+
+
+
+
+
+
 
 
 
