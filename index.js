9e336b793b5f0b5d3b003889e8e7c3de517adf2d
Tima Ivanov <ivtima.bp@gmail.com>
2022-10-04 21:44:43 +0200

    hw_27

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ $.get(
 ).then(data => JSON.parse(data))
     .then(data => {
         const block = $('.block');
-        console.log(block)
 
-        block.innerHTML = '';
+        block.html(data.map(item => `<button id="${item.img}" class="${item.title}" style="background:${item.color}"</button>`));
 
-        const button = data.map(item => `<button style="background:${item.color}"</button>`);
-        block.innerHTML = button;
-
-
-
+        const button = $('button').each(function (i){
+                this.addEventListener('click', changeCar)
+        })
 });
 
-//
+const changeCar = (e) => {
+        const url = 'https://mc-astro.github.io/tesla-roadster-colors/img/';
+        $("img").attr("src", url + `${e.target.id}.jpg`);
+        console.log(e.target.classList)
+        $('.color-div').html(`<p style="color:${e.target.style.background}">Colors</p>`);
+        $('.title-div').html(`<p>${e.target.classList}</p>`);
+
+};
+
+$('.color-div').html(`<p style="color:"#2e2e2e">Colors</p>`);
+$('.title-div').html(`<p>Solid Black</p>`);
+$("img").attr("src", `https://mc-astro.github.io/tesla-roadster-colors/img/black.jpg`);
 
 
 
@@ -22,8 +30,8 @@ $.get(
 
 
 
-// const url = 'https://mc-astro.github.io/tesla-roadster-colors/img/white.jpg';
-// $("img").attr("src", url);
+
+
 
 
 
